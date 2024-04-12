285c503ff964b6193c97f747a497adb76816e809
Milad Alghosi <javad.alghosi7904@gmail.com>
2024-04-12 17:05:22 +0330

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { ReactNode } from 'react';
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
